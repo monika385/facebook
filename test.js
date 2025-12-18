acd7049c59e7135bd0be8fb7261b98dd5aa9246f
monika <monika@cosmoops.com>
2025-12-18 10:56:52 +0530

    variable methods

---
+++ test.js
@@ -0,0 +1,22 @@
+<script>
+    console.log("hello world");
+    console.log(x==5);
+    console.log(x===5);
+
+    const company = "Google";
+let employees = 100;
+
+employees += 20;
+
+console.log(company);  
+console.log(employees);  
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+const x = 5;
+const y = 6;
+const z = x + y;
+
+</script>
